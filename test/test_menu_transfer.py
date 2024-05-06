9a0a0b85f9ee67f98a262c6219bdd8da08ae2373
PavelP78 <166494216+PavelP78@users.noreply.github.com>
2024-05-07 00:22:47 +0300

    Update test_menu_transfer.py

--- test/test_menu_transfer.py
+++ test/test_menu_transfer.py
@@ -1,44 +1,44 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-from config import URL
-from data import get_sign_up_data
+from data import StellarBurgersUrl as Url
+from data import StellarBurgersLogin as Login
 from locators import StellarBurgersLocators
+from helpers import get_sign_up_data
 
 class TestStellarBurgersMenuTransfer:
 
     def test_menu_transfer_personal_account(self, driver):
         driver.find_element(*StellarBurgersLocators.MAIN_ENTER_BUTTON).click()
-        driver.find_element(*StellarBurgersLocators.EMAIL_FIELD).send_keys("pavel_pliasunov_8_888@yandex.ru")
-        driver.find_element(*StellarBurgersLocators.PASSWORD_FIELD).send_keys("12345678")
+        driver.find_element(*StellarBurgersLocators.EMAIL_FIELD).send_keys(*Login.MY_LOGIN)
+        driver.find_element(*StellarBurgersLocators.PASSWORD_FIELD).send_keys(*Login.MY_PASSWORD)
         driver.find_element(*StellarBurgersLocators.ENTER_BUTTON).click()
         driver.find_element(*StellarBurgersLocators.PERSONAL_ACCOUNT_BUTTON).click()
-        assert driver.current_url == f'{URL}account', "Url is wrong"
+        assert driver.current_url == f'{Url.URL_ACCOUNT}', "Url is wrong"
 
     def test_menu_transfer_personal_account_to_design(self, driver):
         driver.find_element(*StellarBurgersLocators.MAIN_ENTER_BUTTON).click()
-        driver.find_element(*StellarBurgersLocators.EMAIL_FIELD).send_keys("pavel_pliasunov_8_888@yandex.ru")
-        driver.find_element(*StellarBurgersLocators.PASSWORD_FIELD).send_keys("12345678")
+        driver.find_element(*StellarBurgersLocators.EMAIL_FIELD).send_keys(*Login.MY_LOGIN)
+        driver.find_element(*StellarBurgersLocators.PASSWORD_FIELD).send_keys(*Login.MY_PASSWORD)
         driver.find_element(*StellarBurgersLocators.ENTER_BUTTON).click()
         driver.find_element(*StellarBurgersLocators.PERSONAL_ACCOUNT_BUTTON).click()
         driver.find_element(*StellarBurgersLocators.DESIGNER_BUTTON).click()
-        assert driver.current_url == f'{URL}', "Url is wrong"
+        assert driver.current_url == f'{Url.URL_MAIN}', "Url is wrong"
 
     def test_menu_exit_from_account(self, driver):
         driver.find_element(*StellarBurgersLocators.MAIN_ENTER_BUTTON).click()
-        driver.find_element(*StellarBurgersLocators.EMAIL_FIELD).send_keys("pavel_pliasunov_8_888@yandex.ru")
-        driver.find_element(*StellarBurgersLocators.PASSWORD_FIELD).send_keys("12345678")
+        driver.find_element(*StellarBurgersLocators.EMAIL_FIELD).send_keys(*Login.MY_LOGIN)
+        driver.find_element(*StellarBurgersLocators.PASSWORD_FIELD).send_keys(*Login.MY_PASSWORD)
         driver.find_element(*StellarBurgersLocators.ENTER_BUTTON).click()
         driver.find_element(*StellarBurgersLocators.PERSONAL_ACCOUNT_BUTTON).click()
         WebDriverWait(driver, 15).until(expected_conditions.visibility_of_element_located(StellarBurgersLocators.EXIT_BUTTON))
         driver.find_element(*StellarBurgersLocators.EXIT_BUTTON).click()
-        assert driver.current_url == f'{URL}account/profile', "Url is wrong"
+        assert driver.current_url == f'{Url.URL_ACCOUNT_PROFILE}', "Url is wrong"
 
     def test_menu_transfer_logo(self, driver):
         driver.find_element(*StellarBurgersLocators.MAIN_ENTER_BUTTON).click()
-        driver.find_element(*StellarBurgersLocators.EMAIL_FIELD).send_keys("pavel_pliasunov_8_888@yandex.ru")
-        driver.find_element(*StellarBurgersLocators.PASSWORD_FIELD).send_keys("12345678")
+        driver.find_element(*StellarBurgersLocators.EMAIL_FIELD).send_keys(*Login.MY_LOGIN)
+        driver.find_element(*StellarBurgersLocators.PASSWORD_FIELD).send_keys(*Login.MY_PASSWORD)
         driver.find_element(*StellarBurgersLocators.ENTER_BUTTON).click()
         driver.find_element(*StellarBurgersLocators.PERSONAL_ACCOUNT_BUTTON).click()
         driver.find_element(*StellarBurgersLocators.LOGO_BUTTON).click()
