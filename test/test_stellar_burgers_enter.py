eb653f291e5ebb28342dc48d63a4d06d2f8ec6f4
PavelP78 <166494216+PavelP78@users.noreply.github.com>
2024-05-07 00:23:17 +0300

    Update test_stellar_burgers_enter.py

--- test/test_stellar_burgers_enter.py
+++ test/test_stellar_burgers_enter.py
@@ -1,17 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-from config import URL
-from data import get_sign_up_data
+from data import StellarBurgersUrl as Url
+from data import StellarBurgersLogin as Login
 from locators import StellarBurgersLocators
+from helpers import get_sign_up_data
 
 class TestStellarBurgersEnter:
 
     def test_main_enter(self, driver):
         driver.find_element(*StellarBurgersLocators.MAIN_ENTER_BUTTON).click()
-        driver.find_element(*StellarBurgersLocators.EMAIL_FIELD).send_keys("pavel_pliasunov_8_888@yandex.ru")
-        driver.find_element(*StellarBurgersLocators.PASSWORD_FIELD).send_keys("12345678")
+        driver.find_element(*StellarBurgersLocators.EMAIL_FIELD).send_keys(*Login.MY_LOGIN)
+        driver.find_element(*StellarBurgersLocators.PASSWORD_FIELD).send_keys(*Login.MY_PASSWORD)
         driver.find_element(*StellarBurgersLocators.ENTER_BUTTON).click()
         WebDriverWait(driver, 15).until(expected_conditions.visibility_of_element_located(StellarBurgersLocators.ORDER_BUTTON))
         order_button = driver.find_element(*StellarBurgersLocators.ORDER_BUTTON)
@@ -19,8 +19,8 @@ class TestStellarBurgersEnter:
 
     def test_personal_account_enter(self, driver):
         driver.find_element(*StellarBurgersLocators.PERSONAL_ACCOUNT_BUTTON).click()
-        driver.find_element(*StellarBurgersLocators.EMAIL_FIELD).send_keys("pavel_pliasunov_8_888@yandex.ru")
-        driver.find_element(*StellarBurgersLocators.PASSWORD_FIELD).send_keys("12345678")
+        driver.find_element(*StellarBurgersLocators.EMAIL_FIELD).send_keys(*Login.MY_LOGIN)
+        driver.find_element(*StellarBurgersLocators.PASSWORD_FIELD).send_keys(*Login.MY_PASSWORD)
         driver.find_element(*StellarBurgersLocators.ENTER_BUTTON).click()
         WebDriverWait(driver, 15).until(expected_conditions.visibility_of_element_located(StellarBurgersLocators.ORDER_BUTTON))
         order_button = driver.find_element(*StellarBurgersLocators.ORDER_BUTTON)
@@ -30,8 +30,8 @@ class TestStellarBurgersEnter:
         driver.find_element(*StellarBurgersLocators.PERSONAL_ACCOUNT_BUTTON).click()
         driver.find_element(*StellarBurgersLocators.REGISTRATION_BUTTON).click()
         driver.find_element(*StellarBurgersLocators.ENTER_BUTTON_REGISTRATION_FORM).click()
-        driver.find_element(*StellarBurgersLocators.EMAIL_FIELD).send_keys("pavel_pliasunov_8_888@yandex.ru")
-        driver.find_element(*StellarBurgersLocators.PASSWORD_FIELD).send_keys("12345678")
+        driver.find_element(*StellarBurgersLocators.EMAIL_FIELD).send_keys(*Login.MY_LOGIN)
+        driver.find_element(*StellarBurgersLocators.PASSWORD_FIELD).send_keys(*Login.MY_PASSWORD)
         driver.find_element(*StellarBurgersLocators.ENTER_BUTTON).click()
         WebDriverWait(driver, 15).until(expected_conditions.visibility_of_element_located(StellarBurgersLocators.ORDER_BUTTON))
         order_button = driver.find_element(*StellarBurgersLocators.ORDER_BUTTON)
@@ -41,8 +41,8 @@ class TestStellarBurgersEnter:
         driver.find_element(*StellarBurgersLocators.MAIN_ENTER_BUTTON).click()
         driver.find_element(*StellarBurgersLocators.BUTTON_PASSWORD_RECOVERY_FORM).click()
         driver.find_element(*StellarBurgersLocators.ENTER_BUTTON_PASSWORD_RECOVERY_FORM).click()
-        driver.find_element(*StellarBurgersLocators.EMAIL_FIELD).send_keys("pavel_pliasunov_8_888@yandex.ru")
-        driver.find_element(*StellarBurgersLocators.PASSWORD_FIELD).send_keys("12345678")
+        driver.find_element(*StellarBurgersLocators.EMAIL_FIELD).send_keys(*Login.MY_LOGIN)
+        driver.find_element(*StellarBurgersLocators.PASSWORD_FIELD).send_keys(*Login.MY_PASSWORD)
         driver.find_element(*StellarBurgersLocators.ENTER_BUTTON).click()
         WebDriverWait(driver, 15).until(expected_conditions.visibility_of_element_located(StellarBurgersLocators.ORDER_BUTTON))
         order_button = driver.find_element(*StellarBurgersLocators.ORDER_BUTTON)
