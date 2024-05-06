4143094f1f29561ccb8171f81a4d4e3b4d44e12a
PavelP78 <166494216+PavelP78@users.noreply.github.com>
2024-05-07 00:21:56 +0300

    Update test_menu_design.py

--- test/test_menu_design.py
+++ test/test_menu_design.py
@@ -1,19 +1,19 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-from config import URL
-from data import get_sign_up_data
+from data import StellarBurgersUrl as Url
+from data import StellarBurgersLogin as Login
 from locators import StellarBurgersLocators
+from helpers import get_sign_up_data
 
 class TestStellarBurgersMenuDesign:
 
     def test_menu_design_rolls(self, driver):
         driver.find_element(*StellarBurgersLocators.SOUCE_BUTTON).click()
         driver.find_element(*StellarBurgersLocators.ROLLS_BUTTON).click()
-        WebDriverWait(driver, 15).until(expected_conditions.visibility_of_element_located(StellarBurgersLocators.ROLLS_PICTURE))
-        rolls_picture = driver.find_element(*StellarBurgersLocators.ROLLS_PICTURE)
-        assert rolls_picture.is_displayed()
+        WebDriverWait(driver, 15).until(expected_conditions.visibility_of_element_located(StellarBurgersLocators.ROLLS_ELEMENT))
+        rolls_element = driver.find_element(*StellarBurgersLocators.ROLLS_ELEMENT)
+        assert rolls_element.is_displayed()
 
     def test_menu_design_souce(self, driver):
         driver.find_element(*StellarBurgersLocators.SOUCE_BUTTON).click()
