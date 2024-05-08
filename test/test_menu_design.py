c434af2ab371fbae77b1f11afd1ba722561dc806
PavelP78 <166494216+PavelP78@users.noreply.github.com>
2024-05-08 12:42:56 +0300

    Update test_menu_design.py

--- test/test_menu_design.py
+++ test/test_menu_design.py
@@ -4,25 +4,24 @@ from selenium.webdriver.support.wait import WebDriverWait
 from data import StellarBurgersUrl as Url
 from data import StellarBurgersLogin as Login
 from locators import StellarBurgersLocators
-from helpers import get_sign_up_data
+
 
 class TestStellarBurgersMenuDesign:
 
+
     def test_menu_design_rolls(self, driver):
-        driver.find_element(*StellarBurgersLocators.SOUCE_BUTTON).click()
-        driver.find_element(*StellarBurgersLocators.ROLLS_BUTTON).click()
-        WebDriverWait(driver, 15).until(expected_conditions.visibility_of_element_located(StellarBurgersLocators.ROLLS_ELEMENT))
-        rolls_element = driver.find_element(*StellarBurgersLocators.ROLLS_ELEMENT)
-        assert rolls_element.is_displayed()
+        driver.find_element(*StellarBurgersLocators.CURRENT_TAB)
+        active_tab = driver.find_element(*StellarBurgersLocators.CURRENT_TAB).text
+        assert active_tab == 'Булки'
 
     def test_menu_design_souce(self, driver):
         driver.find_element(*StellarBurgersLocators.SOUCE_BUTTON).click()
-        WebDriverWait(driver, 15).until(expected_conditions.visibility_of_element_located(StellarBurgersLocators.SOUCE_PICTURE))
-        souce_picture = driver.find_element(*StellarBurgersLocators.SOUCE_PICTURE)
-        assert souce_picture.is_displayed()
+        driver.find_element(*StellarBurgersLocators.CURRENT_TAB)
+        active_tab = driver.find_element(*StellarBurgersLocators.CURRENT_TAB).text
+        assert active_tab == 'Соусы'
 
     def test_menu_design_filling(self, driver):
-        driver.find_element(*StellarBurgersLocators.FILLING_BUTTON).click()
-        WebDriverWait(driver, 15).until(expected_conditions.visibility_of_element_located(StellarBurgersLocators.FILLING_PICTURE))
-        filling_picture = driver.find_element(*StellarBurgersLocators.FILLING_PICTURE)
-        assert filling_picture.is_displayed()
+         driver.find_element(*StellarBurgersLocators.FILLING_BUTTON).click()
+         driver.find_element(*StellarBurgersLocators.CURRENT_TAB)
+         active_tab = driver.find_element(*StellarBurgersLocators.CURRENT_TAB).text
+         assert active_tab == 'Начинки'
